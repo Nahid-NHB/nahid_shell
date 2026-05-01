faa744ec7ebf5c6cbb834180a8b7e2df6bd1bc4e
Md Nahid Hassan Bhuiyan <nahid151341@gmail.com>
2026-05-01 20:34:32 +0600

    basic setup with sell_loop

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "nahid_shell.h"
+
+void shell_loop(char **env){
+
+}
+
+int main(int argc, char **argv, char **env){
+    (void)argc;
+    (void)argv;
+
+    shell_loop(env);
+    
+    return 0;
+}
